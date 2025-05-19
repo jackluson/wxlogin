1047534da35dcea5a3493831f93fab807c3af02d
jackluson <luxuemin2108@gmail.com>
2025-05-20 00:39:49 +0800

    fix create order

--- src/pages/api/pay/create.ts
+++ src/pages/api/pay/create.ts
@@ -6,10 +6,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse, user:TokenPayl
   if(req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
-  const { appKey } = req.body
+  const { planKey } = req.body
   const userId = user.openid
   const notifyUrl = process.env.NOTIFY_URL!
-  const payOrder = await SDK.pay.createPay(appKey, userId, notifyUrl)
+  const payOrder = await SDK.pay.createPay(planKey, userId, notifyUrl)
   return res.status(200).json({
     success: true,
     order: payOrder,
